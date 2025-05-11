20abfcc178f56bf745a648899f3c03de8c9a5241
nisha <2016ume1288@mnit.ac.in>
2025-05-11 12:41:32 +0530

    get code

--- main.py
+++ main.py
@@ -10,4 +10,9 @@ CLIENT_ID     = os.getenv("SPOTIFY_CLIENT_ID")
 CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
 REFRESH_TOKEN = os.getenv("SPOTIFY_REFRESH_TOKEN")
 REDIRECT_URI  = os.getenv("SPOTIFY_REDIRECT_URI")
+
+@app.get("/auth/spotify/callback")
+def spotify_callback(code: str):
+    return {"code": code}
+
  
