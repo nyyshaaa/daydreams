54f8ff8d32878e50548c1254ce9f647c07173639
nisha <2016ume1288@mnit.ac.in>
2025-05-11 12:15:47 +0530

    deploy

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+import time
+import requests
+from fastapi import FastAPI, HTTPException, Path
+from pydantic import BaseModel
+
+app = FastAPI()
+
+CLIENT_ID     = os.getenv("SPOTIFY_CLIENT_ID")
+CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+REFRESH_TOKEN = os.getenv("SPOTIFY_REFRESH_TOKEN")
+REDIRECT_URI  = os.getenv("SPOTIFY_REDIRECT_URI")
