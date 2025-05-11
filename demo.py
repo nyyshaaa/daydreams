59509485bbec81c35a9a827e1c9dbe39e2303964
Nisha <88261286+nyyshaaa@users.noreply.github.com>
2025-05-11 14:25:52 +0530

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,117 @@
+demo.py
+
+/spotify/followed-artists
+
+{
+  "artists": {
+    "href": "https://api.spotify.com/v1/me/following?type=artist&limit=20",
+    "limit": 20,
+    "next": null,
+    "cursors": {
+      "after": null
+    },
+    "total": 3,
+    "items": [
+      {
+        "external_urls": {
+          "spotify": "https://open.spotify.com/artist/23CbYFymkUfLkR5AqGS3yV"
+        },
+        "followers": {
+          "href": null,
+          "total": 297
+        },
+        "genres": [],
+        "href": "https://api.spotify.com/v1/artists/23CbYFymkUfLkR5AqGS3yV",
+        "id": "23CbYFymkUfLkR5AqGS3yV",
+        "images": [
+          {
+            "url": "https://i.scdn.co/image/ab6761610000e5ebc76b3dfb629b4b7d21ddc282",
+            "height": 640,
+            "width": 640
+          },
+          {
+            "url": "https://i.scdn.co/image/ab67616100005174c76b3dfb629b4b7d21ddc282",
+            "height": 320,
+            "width": 320
+          },
+          {
+            "url": "https://i.scdn.co/image/ab6761610000f178c76b3dfb629b4b7d21ddc282",
+            "height": 160,
+            "width": 160
+          }
+        ],
+        "name": "Jennifer Foster",
+        "popularity": 25,
+        "type": "artist",
+        "uri": "spotify:artist:23CbYFymkUfLkR5AqGS3yV"
+      },
+      {
+        "external_urls": {
+          "spotify": "https://open.spotify.com/artist/6tFIXOQGf0Hld1gCjBkLaw"
+        },
+        "followers": {
+          "href": null,
+          "total": 8598
+        },
+        "genres": [],
+        "href": "https://api.spotify.com/v1/artists/6tFIXOQGf0Hld1gCjBkLaw",
+        "id": "6tFIXOQGf0Hld1gCjBkLaw",
+        "images": [
+          {
+            "url": "https://i.scdn.co/image/ab6761610000e5ebd41cf4f529fcb34ade25de3d",
+            "height": 640,
+            "width": 640
+          },
+          {
+            "url": "https://i.scdn.co/image/ab67616100005174d41cf4f529fcb34ade25de3d",
+            "height": 320,
+            "width": 320
+          },
+          {
+            "url": "https://i.scdn.co/image/ab6761610000f178d41cf4f529fcb34ade25de3d",
+            "height": 160,
+            "width": 160
+          }
+        ],
+        "name": "fwd/slash",
+        "popularity": 49,
+        "type": "artist",
+        "uri": "spotify:artist:6tFIXOQGf0Hld1gCjBkLaw"
+      },
+      {
+        "external_urls": {
+          "spotify": "https://open.spotify.com/artist/04m3oUGzjO3EJTQidFzTgM"
+        },
+        "followers": {
+          "href": null,
+          "total": 94862
+        },
+        "genres": [
+          "tropical house"
+        ],
+        "href": "https://api.spotify.com/v1/artists/04m3oUGzjO3EJTQidFzTgM",
+        "id": "04m3oUGzjO3EJTQidFzTgM",
+        "images": [
+          {
+            "url": "https://i.scdn.co/image/ab6761610000e5ebe8898b816e2b316438ea64c6",
+            "height": 640,
+            "width": 640
+          },
+          {
+            "url": "https://i.scdn.co/image/ab67616100005174e8898b816e2b316438ea64c6",
+            "height": 320,
+            "width": 320
+          },
+          {
+            "url": "https://i.scdn.co/image/ab6761610000f178e8898b816e2b316438ea64c6",
+            "height": 160,
+            "width": 160
+          }
+        ],
+        "name": "Nora Van Elken",
+        "popularity": 58,
+        "type": "artist",
+        "uri": "spotify:artist:04m3oUGzjO3EJTQidFzTgM"
+      }
+    ]
+  }
